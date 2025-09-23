84dd2c4df42511b5ba0bdc2b570e3cd7293162be
soumya05shivankar <159751734+soumya05shivankar@users.noreply.github.com>
2025-09-23 10:41:15 +0530

    Add files via upload

---
+++ practical_10.R
@@ -0,0 +1,14 @@
+#Soumya_Shivankar_22070521177
+#Practical_10
+before <- c(80, 75, 90, 85, 95, 100, 70, 65, 88, 92)
+after  <- c(78, 73, 86, 82, 91, 97, 69, 64, 85, 89)
+
+diff <- before - after
+mean_diff <- mean(diff)
+sd_diff <- sd(diff)
+n <- length(diff)
+t_value <- mean_diff / (sd_diff / sqrt(n))
+t_value   
+
+t_test_result <- t.test(before, after, paired = TRUE, alternative = "greater")
+print(t_test_result)
